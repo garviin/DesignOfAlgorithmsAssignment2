6fe2deb75f7f10a54200a1b40976c51bc33a9140
garviin <garvibulkin@gmail.com>
2019-05-19 12:35:23 +1000

    implemented problem 1b

--- p1.c
+++ p1.c
@@ -10,6 +10,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define RIGHT_MAX_HEAP 1
+#define MAX_HEAP 0
+
+// Function prototypes
+int read_input_size();
+int *read_input();
+void build_max_heap(int* integers, int size, int isRightHeap);
+void max_heapify(int* integers, int size, int parent, int isRightHeap);
+void swap_node(int* a, int* b);
+void print_array(int *integers, int size);
+
 /* --- DO NOT CHANGE THE CODE BELOW THIS LINE --- */
 
 void problem_1_a();
@@ -44,10 +55,90 @@ void print_usage_and_exit(char **argv) {
 
 /* TODO: Implement your solution to Problem 1.a. in this function. */
 void problem_1_a() {
-
+  int N = read_input_size();
+  int *integers = read_input(N);
+  build_max_heap(integers, N, MAX_HEAP);
+  print_array(integers, N);
+  free(integers);
 }
+
 
 /* TODO: Implement your solution to Problem 1.b. in this function. */
 void problem_1_b() {
+  int N = read_input_size();
+  int *integers = read_input(N);
+  build_max_heap(integers, N, RIGHT_MAX_HEAP);
+  print_array(integers, N);
+  free(integers);
+}
 
+/* Helper functions */
+
+void build_max_heap(int* integers, int size, int isRightHeap){
+  for(int i = (size)/2 - 1; i >= 0; i--){
+    max_heapify(integers, size, i, isRightHeap);
+  }
+}
+
+void max_heapify(int* integers, int size, int parent, int isRightHeap){
+  // Initialize indexes for the current tree
+  int largest_node = parent;
+  int left_child = 2*parent + 1;
+  int right_child = 2*parent + 2;
+
+  // Getting largest element
+  if(left_child < size && integers[left_child] > integers[parent]){
+    largest_node = left_child;
+  }
+  if(right_child < size && integers[right_child] > integers[largest_node]){
+    largest_node = right_child;
+  }
+
+  // swap with child if parent is not the largest
+  if(largest_node !=  parent){
+    swap_node(&integers[parent], &integers[largest_node]);
+    max_heapify(integers, size, largest_node, isRightHeap);
+  }
+
+  // For creating right max heaps. Swaps the children if right child is smaller than the left.
+  if(isRightHeap == RIGHT_MAX_HEAP &&
+    right_child < size &&
+    left_child < size &&
+    integers[left_child] > integers[right_child]){
+      swap_node(&integers[left_child], &integers[right_child]);
+    }
+}
+
+// Function to swap values of two pointers
+void swap_node(int *a, int *b){
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+// Reads the input size
+int read_input_size(){
+  int N;
+  scanf("%d", &N);
+  return N;
+}
+
+// Reads N inputs, and returns a pointer array for the inputs
+int *read_input(int N){
+  // intialize pointer for an array of size N
+  int *integers = (int *)malloc(N*sizeof(int));
+
+  // Puts the input values in the array
+  for (int i = 0; i < N; i++){
+    scanf("%d", integers + i);
+  }
+
+  return integers;
+}
+
+// Prints an array of integers
+void print_array(int *integers, int size){
+  for (int i = 0; i < size; i++){
+    printf("%d\n", *(integers + i));
+  } 
 }
